760461002795170c863574117b1a8205771e19db
Gaurav Kumar <gauravkmadhubani@gmail.com>
2023-07-27 12:14:59 +0530

    file structure changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Home from './pages/Home'
 import Other from './pages/Other'
+import Vans from './pages/Vans'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
@@ -13,11 +14,13 @@ function App() {
           <Link className="site-logo" to="/">#VanLife</Link>
           <nav>
             <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
         </header>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<Other />} />
+          <Route path='/vans' element={<Vans />} />
         </Routes>
       </BrowserRouter>
     </>
